9286bbd1b0ad6193e7f133555290ec285ee13d96
jjy321501 <jjy321501@naver.com>
2022-08-10 00:06:11 +0900

    useParams / useSearchParams / useNavigate

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,8 +1,35 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSeachParams] = useSearchParams();
+  const id = searchParams.get("id");
+
+  console.log("id :", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode :", mode);
   return (
     <div>
       <h1>Edit</h1>
       <p>이 곳은 수정 페이지 입니다.</p>
+      <button onClick={() => setSeachParams({ who: "정준영" })}>
+        QS 바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        홈으로가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
